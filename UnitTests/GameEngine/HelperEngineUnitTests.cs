abcd97dc7335eb83dc8dabc9cd3edf7ec18e7137
brandiweekes <brandiweekes@gmail.com>
2019-02-13 21:27:26 -0800

    RollDice w/o fixed returns are passing with 100% coverage (#4-5 UT assignment)

--- UnitTests/GameEngine/HelperEngineUnitTests.cs
+++ UnitTests/GameEngine/HelperEngineUnitTests.cs
@@ -27,43 +27,84 @@ namespace UnitTests.GameEngine
         [Test]
         public void RollDice_Roll_2_Dice_10_Should_Pass()
         {
+            // Arrange
             var Roll = 2;
             var Dice = 10;
-            var Expected = 2;
-            
 
             // Act
             var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
 
-            // Reset
-            Crawl.Models.GameGlobals.ToggleRandomState();
-
             // Assert
-            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+            Assert.NotZero(Actual, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
         public void RollDice_Roll_0_Dice_10_Should_Fail()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var Roll = 0;
+            var Dice = 10;
+
+            // Act
+            var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
+
+            // Assert
+            Assert.Zero(Actual, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
         public void RollDice_Roll_Neg1_Dice_10_Should_Fail()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var Roll = -1;
+            var Dice = 10;
+
+            // Act
+            var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
+
+            // Assert
+            Assert.Zero(Actual, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
         public void RollDice_Roll_1_Dice_Neg1_Should_Fail()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var Roll = 1;
+            var Dice = -1;
+
+            // Act
+            var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
+
+            // Assert
+            Assert.Zero(Actual, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
         public void RollDice_Roll_1_Dice_0_Should_Fail()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var Roll = 1;
+            var Dice = 0;
+
+            // Act
+            var Actual = Crawl.GameEngine.HelperEngine.RollDice(Roll, Dice);
+
+            // Assert
+            Assert.Zero(Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void RollDice_Roll_1_Dice_10_Fixed_Should_Return_5()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Reset
+            //Crawl.Models.GameGlobals.ToggleRandomState();
+
+            // Assert
         }
     }
 
